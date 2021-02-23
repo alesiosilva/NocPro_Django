97907ccf94d8d2a53264873920319e314ce922ef
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-02-22 21:26:04 -0300

    Criação do projeto,app core e login

---
+++ apps/core/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def home(request):
+    data = {}
+    data['usuario'] = request.user
+    return render(request, 'core/index.html', data)
