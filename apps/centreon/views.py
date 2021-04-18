8d596a9e6f09e1f40d8ea66c8fda7e19237a368a
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-04-18 00:53:30 -0300

    corrigir problema de timeout e problema de servidor não cadastrado

--- apps/centreon/views.py
+++ apps/centreon/views.py
@@ -6,16 +6,17 @@ import requests
 from django.contrib.auth.decorators import login_required
 from apps.core.models import Servers
 
-if Servers.objects.filter(nome="centreon"):
-    model_servers = Servers.objects.get(nome="centreon")
-    centreon_servidor = model_servers.host
-    centreon_api_user = model_servers.user
-    centreon_api_senha = model_servers.password
+#if Servers.objects.filter(nome="centreon"):
+#    model_servers = Servers.objects.get(nome="centreon")
+#    centreon_servidor = model_servers.host
+#    centreon_api_user = model_servers.user
+#    centreon_api_senha = model_servers.password
 
 def autenticar():
     try:
-        response = requests.post(centreon_servidor + '/centreon/api/index.php?action=authenticate',
-        data={'username': centreon_api_user, 'password': centreon_api_senha})
+        model_servers = Servers.objects.get(nome="centreon")
+        response = requests.post(model_servers.host + '/centreon/api/index.php?action=authenticate',
+        data={'username': model_servers.user, 'password': model_servers.password})
         token = response.json()
         return token['authToken']
     except requests.exceptions.ConnectionError:
@@ -23,34 +24,37 @@ def autenticar():
 
 @login_required
 def host_list(request):
-    if Servers.objects.filter(nome="centreon"):
         try:
-            response = requests.get(centreon_servidor + '/centreon/api/index.php?object=centreon_realtime_hosts&sortType=id&order=desc&action=list&fields=id,name,notes,address',
-            headers={'content-type': 'application/json', 'centreon-auth-token': autenticar()})
-            hosts = response.json()
-            return render(request, 'centreon/host_list.html', {
-                'host_list': hosts
-            })
+            model_servers = Servers.objects.get(nome="centreon")
+            if Servers.objects.filter(nome="centreon"):
+                response = requests.get(model_servers.host + '/centreon/api/index.php?object=centreon_realtime_hosts&sortType=id&order=desc&action=list&fields=id,name,notes,address',
+                headers={'content-type': 'application/json', 'centreon-auth-token': autenticar()})
+                hosts = response.json()
+                return render(request, 'centreon/host_list.html', {
+                    'host_list': hosts
+                })
+            else:
+                return HttpResponseRedirect("/servers_create")
+
         except requests.exceptions.ConnectionError as e:
             return HttpResponse("Erro ao conectar no servidor")
-    else:
-        return HttpResponseRedirect("/servers_create")
 
 
 def host_field():
-    if Servers.objects.filter(nome="centreon"):
         try:
-            response = requests.get(centreon_servidor + '/centreon/api/index.php?object=centreon_realtime_hosts&sortType=id&order=desc&action=list&fields=id,name,notes,address',
-            headers={'content-type': 'application/json', 'centreon-auth-token': autenticar()})
-            hosts = response.json()
-            #return hosts[1]['id']
-            return hosts
+            model_servers = Servers.objects.get(nome="centreon")
+            if Servers.objects.filter(nome="centreon"):
+                response = requests.get(model_servers.host + '/centreon/api/index.php?object=centreon_realtime_hosts&sortType=id&order=desc&action=list&fields=id,name,notes,address',
+                headers={'content-type': 'application/json', 'centreon-auth-token': autenticar()})
+                hosts = response.json()
+                #return hosts[1]['id']
+                return hosts
+            else:
+                # return HttpResponseRedirect("servers_create")
+                hosts = ()
+                return hosts
         except requests.exceptions.ConnectionError:
             hosts = ()
             return hosts
-    else:
-        #return HttpResponseRedirect("servers_create")
-        hosts = ()
-        return hosts
 
 
--- apps/topdesk/views.py
+++ apps/topdesk/views.py
@@ -1,31 +1,44 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
 from django.contrib.auth.decorators import login_required
 import base64
 from apps.core.models import Servers
 
-if Servers.objects.filter(nome="topdesk"):
-    model_servers = Servers.objects.get(nome="topdesk")
-    servidor = model_servers.host
-    api_user = model_servers.user
-    api_senha = model_servers.password
-    data_string = api_user + ":" + api_senha
-    authorization = data_string.encode("utf-8")
-    authorization = base64.b64encode(authorization)
-    authorization = authorization.decode("utf-8")
-    authorization = "Basic " + str(authorization)
+#if Servers.objects.filter(nome="topdesk"):
+#    model_servers = Servers.objects.get(nome="topdesk")
+#    servidor = model_servers.host
+#    api_user = model_servers.user
+#    api_senha = model_servers.password
+#    data_string = api_user + ":" + api_senha
+#    authorization = data_string.encode("utf-8")
+#    authorization = base64.b64encode(authorization)
+#    authorization = authorization.decode("utf-8")
+#    authorization = "Basic " + str(authorization)
 
 
 @login_required
 def ativos_list(request):
-    if Servers.objects.filter(nome="topdesk"):
-        response = requests.get(servidor + '/tas/api/assetmgmt/assets?fields=unid,specification,name&templateName=Backbone',
-        #headers={'content-type': 'application/json', 'Authorization': 'Basic xxxxxxxxxxx'})
-        headers={'content-type': 'application/json', 'Authorization': authorization})
-        ativos = response.json()
-        return render(request, 'topdesk/ativos_list.html', {
-            'ativos_list': ativos['dataSet']
-        })
-    else:
-        return HttpResponseRedirect("/servers_create")
+    try:
+        model_servers = Servers.objects.get(nome="topdesk")
+        #servidor = model_servers.host
+        api_user = model_servers.user
+        api_senha = model_servers.password
+        data_string = api_user + ":" + api_senha
+        authorization = data_string.encode("utf-8")
+        authorization = base64.b64encode(authorization)
+        authorization = authorization.decode("utf-8")
+        authorization = "Basic " + str(authorization)
+        if Servers.objects.filter(nome="topdesk"):
+            response = requests.get(model_servers.host + '/tas/api/assetmgmt/assets?fields=unid,specification,name&templateName=Backbone',
+            #headers={'content-type': 'application/json', 'Authorization': 'Basic xxxxxxxxxxx'})
+            headers={'content-type': 'application/json', 'Authorization': authorization})
+            ativos = response.json()
+            return render(request, 'topdesk/ativos_list.html', {
+                'ativos_list': ativos['dataSet']
+            })
+        else:
+            return HttpResponseRedirect("/servers_create")
+
+    except requests.exceptions.ConnectionError as e:
+        return HttpResponse("Erro ao conectar no servidor")
