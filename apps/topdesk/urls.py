986a0cbeeca30a824b432419d388a6ab6db65685
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-08-25 22:31:55 -0300

    Criar view de atualização massiva do topdesk

--- apps/topdesk/urls.py
+++ apps/topdesk/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ativos_list
+from .views import ativos_list,massive_change, tickets_update
 
 urlpatterns = [
     #path('', HostList.as_view(), name='list_hosts'),
-    path('', ativos_list, name='ativos_list'),
+    path('ativos', ativos_list, name='ativos_list'),
+    path('', massive_change, name='massive_change'),
+    path('tickets_update', tickets_update, name='tickets_update'),
     ]
--- apps/topdesk/views.py
+++ apps/topdesk/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
@@ -18,6 +19,99 @@ from apps.core.models import Servers
 
 
 @login_required
+def tickets_update(request):
+    inputs_status = request.POST.get('status')
+    inputs_alterar = request.POST.getlist('alterar')
+    #return HttpResponse(inputs_alterar)
+    #response = json.dumps(
+    #    {
+    #        'inputs_status': inputs_status,
+    #        'inputs_alterar': inputs_alterar
+    #    }
+    #)
+    #return HttpResponse(response, content_type='application/json')
+
+    chamados = json.dumps(inputs_alterar)
+
+    for chamado in inputs_alterar:
+        rest_post_update(chamado,inputs_status)
+    #    x = chamado
+    #return HttpResponse(x)
+
+    #return HttpResponse("chamados atualizados")
+    #return HttpResponse(inputs_status)
+
+    return HttpResponse(chamados, content_type='application/json')
+
+
+def rest_post_update(chamado,inputs_status):
+    try:
+        if Servers.objects.filter(nome="topdesk"):
+            model_servers = Servers.objects.get(nome="topdesk")
+            # servidor = model_servers.host
+            api_user = model_servers.user
+            api_senha = model_servers.password
+            data_string = api_user + ":" + api_senha
+            authorization = data_string.encode("utf-8")
+            authorization = base64.b64encode(authorization)
+            authorization = authorization.decode("utf-8")
+            authorization = "Basic " + str(authorization)
+
+            url_complement = '/tas/api/incidents/id/' + chamado
+
+            ticket_create_json = {
+                'processingStatus':
+                    {'name': inputs_status}
+            }
+
+            response = requests.put(model_servers.host + url_complement,
+            headers={'content-type': 'application/json', 'Authorization': authorization},
+            data=json.dumps(ticket_create_json)
+            )
+            tickets = response.json()
+
+            #return render(request, 'topdesk/massive_change.html', {'tickets_list': tickets})
+            return HttpResponse(tickets, content_type='application/json')
+        else:
+            return HttpResponseRedirect("/servers_create")
+
+    except requests.exceptions.ConnectionError as e:
+        return HttpResponse("Erro ao conectar no servidor")
+
+
+@login_required
+def massive_change(request):
+    try:
+        if Servers.objects.filter(nome="topdesk"):
+            model_servers = Servers.objects.get(nome="topdesk")
+            # servidor = model_servers.host
+            api_user = model_servers.user
+            api_senha = model_servers.password
+            data_string = api_user + ":" + api_senha
+            authorization = data_string.encode("utf-8")
+            authorization = base64.b64encode(authorization)
+            authorization = authorization.decode("utf-8")
+            authorization = "Basic " + str(authorization)
+
+            url_complement = '/tas/api/incidents?page_size=20&'
+            url_complement = url_complement + 'query=operatorGroup.name==GTI::NOC::NOC-ADM;'
+            url_complement = url_complement + 'processingStatus.name!=Fechado&'
+            url_complement = url_complement + 'fields=id,number,briefDescription,processingStatus.name'
+
+            response = requests.get(model_servers.host + url_complement,
+            #headers={'content-type': 'application/json', 'Authorization': 'Basic xxxxxxxxxxx'})
+            headers={'content-type': 'application/json', 'Authorization': authorization})
+            tickets = response.json()
+
+            return render(request, 'topdesk/massive_change.html', {'tickets_list': tickets})
+        else:
+            return HttpResponseRedirect("/servers_create")
+
+    except requests.exceptions.ConnectionError as e:
+        return HttpResponse("Erro ao conectar no servidor")
+
+
+@login_required
 def ativos_list(request):
     try:
         if Servers.objects.filter(nome="topdesk"):
@@ -35,9 +129,7 @@ def ativos_list(request):
             #headers={'content-type': 'application/json', 'Authorization': 'Basic xxxxxxxxxxx'})
             headers={'content-type': 'application/json', 'Authorization': authorization})
             ativos = response.json()
-            return render(request, 'topdesk/ativos_list.html', {
-                'ativos_list': ativos['dataSet']
-            })
+            return render(request, 'topdesk/ativos_list.html', {'ativos_list': ativos['dataSet']})
         else:
             return HttpResponseRedirect("/servers_create")
 
